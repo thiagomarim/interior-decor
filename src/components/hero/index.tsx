e1f00dc66ae5489ed4178779f654ecedc8bfb645
Thiago <thiago.marim2005@gmail.com>
2024-05-13 14:25:57 -0300

    refactor: add responsive layout to hero section

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -3,12 +3,12 @@ import Button from "../button";
 export default function Hero() {
   return (
     <section className="bg-hero-image bg-center bg-cover lg:bg-cover bg-no-repeat w-full h-[640px] xl:h-[840px]">
-      <div className="container flex items-center justify-start h-full">
-        <div className="flex flex-col gap-5 max-w-[640px]">
-          <h1 className="font-title text-7xl text-[#1F1F1F]">
+      <div className="container flex items-center justify-center text-center 2xl:text-start 2xl:justify-start h-full">
+        <div className="flex flex-col gap-5 max-w-[640px] items-center 2xl:items-start">
+          <h1 className="font-title text-5xl lg:text-7xl text-[#1F1F1F]">
             Interior Design
           </h1>
-          <p className="text-xl text-[#545454]">
+          <p className="text-lg lg:text-xl text-[#545454]">
             Step into a world where the art of Interior Design is meticulously
             crafted to bring together timeless elegance and cutting-edge modern
             Innovation, Allowing you to transform your living spaces into the
