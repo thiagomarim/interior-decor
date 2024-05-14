9e112b72d8d697306f5c066f9aa277c739a81316
Thiago <thiago.marim2005@gmail.com>
2024-05-14 14:49:13 -0300

    feat: add design section

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import DesignSection from "./components/design";
 import Header from "./components/header";
 import Hero from "./components/hero";
 import Services from "./components/services";
@@ -6,9 +7,10 @@ export default function App() {
   return (
     <div>
       <Header />
-      <main className="max-w-full flex flex-col gap-32">
+      <main className="max-w-full flex flex-col gap-32 sm:gap-40 mb-96">
         <Hero />
         <Services />
+        <DesignSection />
       </main>
     </div>
   );
---
+++ src/components/design/index.tsx
@@ -0,0 +1,40 @@
+import { BsTelephoneFill } from "react-icons/bs";
+import room from "../../assets/room.jpg";
+import Button from "../button";
+
+export default function DesignSection() {
+  return (
+    <section className="container">
+      <div className="grid grid-cols-1 justify-items-center lg:justify-items-start lg:grid-cols-2 gap-28">
+        <div>
+          <img src={room} alt="Design de um quarto" width={711} height={711} />
+        </div>
+        <div className="max-w-[704px] flex flex-col gap-12">
+          <h1 className="text-5xl font-title">
+            Designing Your Dream With Brilliance
+          </h1>
+          <p className="text-secondary text-xl">
+            Elevate your spaces with bespoke interior designs that reflect your
+            unique style and aspirations, crafted with precision and brilliance
+            for an unforgettable living experience
+          </p>
+          <div className="flex items-center gap-6">
+            <span className="size-14 rounded-full bg-terceary/40 flex items-center justify-center">
+              <BsTelephoneFill size={20} color="#fdba74" />
+            </span>
+            <div className="flex flex-col gap-1">
+              <a
+                href="tel:0946967032"
+                className="text-lg font-bold hover:text-secondary transition-colors"
+              >
+                0946 967 032
+              </a>
+              <span className="text-xl text-secondary">Call us any time</span>
+            </div>
+          </div>
+          <Button>See more designs</Button>
+        </div>
+      </div>
+    </section>
+  );
+}
