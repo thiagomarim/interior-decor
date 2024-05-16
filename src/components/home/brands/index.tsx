6c22e5532ff6c4dfb62d9b4a52819d59ec8984db
Thiago <thiago.marim2005@gmail.com>
2024-05-16 15:56:48 -0300

    style: add animation to brands section

--- src/components/home/brands/index.tsx
+++ src/components/home/brands/index.tsx
@@ -4,6 +4,8 @@ import brand from "../../../assets/brands/03.svg";
 import nature from "../../../assets/brands/04.svg";
 import classic from "../../../assets/brands/05.svg";
 
+import { motion } from "framer-motion";
+
 const BRANDS = [
   {
     name: "Mordern Home",
@@ -31,7 +33,16 @@ export default function Brands() {
   return (
     <section className="container grid items-center grid-cols-1 justify-items-center lg:justify-items-start sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 flex-wrap gap-16 lg:gap-32">
       {BRANDS.map((brand, i) => (
-        <img src={brand.src} alt={`Logo da empresa ${brand.name}`} key={i} />
+        <motion.img
+          src={brand.src}
+          alt={`Logo da empresa ${brand.name}`}
+          key={i}
+          initial={{ opacity: 0, y: 150 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 150 }}
+          transition={{ duration: 1.2, delay: i * 0.3 }}
+          viewport={{ once: true }}
+        />
       ))}
     </section>
   );
